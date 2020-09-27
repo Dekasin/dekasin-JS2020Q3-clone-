d1970d0db5590903dfe4336e6d329930ad18f0ff
Dzianis Zelianitski <dekasins@gmail.com>
2020-09-27 14:17:12 +0300

    feat: something works

--- calculator/script.js
+++ calculator/script.js
@@ -1,23 +1,88 @@
 var numbers = document.querySelectorAll('.number'),
 	operators = document.querySelectorAll('.operator'),
-	decimal = document.getElementById('decim'),
-	c = document.getElementById('c'),
-	ce = document.getElementById('ce'),
-	result = document.getElementById('result');
-console.log(result);
+	decimalBtn = document.getElementById('decim'),
+	clearBtns = document.querySelectorAll('.clear-btn'),
+	display = document.getElementById('display'),
+	MemoryCurrentNumber = 0,
+	MemoryNewNumber = false,
+	MemoryPendingOperation = '',
 
-function numberPress(){
+	resultBtn = document.getElementById('result');
 
-};
-function operation(){
-
-};
-function decimal(){
-
-};
-function clear(){
-
-};
-function howWork(){
+for (var i = 0; i < numbers.length; i++) {
+	var number = numbers[i];
+	number.addEventListener('click', function (e) {
+		//console.log(e)
+		numberPress(e.srcElement.innerHTML)
+	});
 
 };
+
+for (var i = 0; i < operators.length; i++) {
+	var operationBtn = operators[i];
+	operationBtn.addEventListener('click', function (e) {
+		operation(e.target.textContent);
+	})
+};
+
+for (var i = 0; i < clearBtns.length; i++) {
+	var clearBtn = clearBtns[i];
+	clearBtn.addEventListener('click', function (e) {
+		clear(e.srcElement.id)
+	})
+};
+
+// resultBtn.addEventListener('click', result);
+
+decimalBtn.addEventListener('click', decimal);
+
+function numberPress(number) {
+	console.log('Click on button with ' + number + '!');
+	if (MemoryNewNumber) {
+		display.value = number;
+		MemoryNewNumber = false;
+	} else {
+		if (display.value === '0') {
+			display.value = number;
+		} else {
+			display.value += number;
+		}
+	}
+	};
+
+	function operation(op) {
+		var localOperationMemory = display.value;
+
+		if (MemoryNewNumber && MemoryPendingOperation !== '=') {
+			display.value = MemoryCurrentNumber;
+		} else {
+			MemoryNewNumber = true;
+			if (MemoryPendingOperation === '+') {
+				MemoryCurrentNumber += parseFloat(localOperationMemory);
+			} else if (MemoryPendingOperation === '-') {
+				MemoryCurrentNumber -= parseFloat(localOperationMemory);
+			} else if (MemoryPendingOperation === '*') {
+				MemoryCurrentNumber *= parseFloat(localOperationMemory);
+			} else if (MemoryPendingOperation === "/") {
+				MemoryCurrentNumber /= parseFloat(localOperationMemory);
+			} else {
+				MemoryCurrentNumber = parseFloat(localOperationMemory);
+			};
+			display.value = MemoryCurrentNumber;
+			MemoryPendingOperation = op;
+		}
+		console.log('Click on button with ' + op + '!');
+	};
+
+	function decimal() {
+		console.log('Click on button decimal!');
+	};
+
+	function clear(id) {
+		console.log('Click on button ' + id + '!');
+
+	};
+
+	// function result() {
+	// 	console.log('Click on button result')
+	// };
