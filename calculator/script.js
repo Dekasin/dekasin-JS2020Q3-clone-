f460d00d5e04a416900eeb3a123f68f383cfe382
Dzianis Zelianitski <dekasins@gmail.com>
2020-09-27 10:37:35 +0300

    feat: add buttons to script

---
+++ calculator/script.js
@@ -0,0 +1,23 @@
+var numbers = document.querySelectorAll('.number'),
+	operators = document.querySelectorAll('.operator'),
+	decimal = document.getElementById('decim'),
+	c = document.getElementById('c'),
+	ce = document.getElementById('ce'),
+	result = document.getElementById('result');
+console.log(result);
+
+function numberPress(){
+
+};
+function operation(){
+
+};
+function decimal(){
+
+};
+function clear(){
+
+};
+function howWork(){
+
+};
